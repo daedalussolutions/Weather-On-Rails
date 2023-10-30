ef317bcc61000c65d96514f06e9f6a693acd5811
daedalussolutions <dylancassell000@hotmail.com>
2023-10-30 18:36:24 -0400

    Geolocation functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
-  def index
-    @data = CurrentWeatherService.new(latitude: "43.6532", longitude:"79.3832", units:"metric").call
+  def location
+    respond_to do |format|
+      format.html # location.html.erb
+      format.json { render json: @place }
+      
+      latitude, longitude = params[:place].split(',')
+
+      puts "Latitude: #{latitude}"
+      puts "Longitude: #{longitude}"
+      
+      @data = CurrentWeatherService.new(latitude: latitude, longitude: longitude, units: "metric").call
+    end
   end
+  def index
+      @data = CurrentWeatherService.new(latitude: "43.6532", longitude: "-79.3832", units: "metric").call
+  end
+
 end
 
