389b83a6298701619d3e9ecf608c9be059bc1639
daedalussolutions <dylancassell000@hotmail.com>
2023-11-01 17:06:58 -0400

    Added About page and updated styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get "about", to: "about#index"
   get "/location" => "home#location"
   get "/search", to: "home#search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
